16432ffb41dd7940f30cd874a3d245439631003a
blhbhl <127571546+blhbhl@users.noreply.github.com>
2023-06-05 23:11:01 +0800

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,10 +7,10 @@ function Banner() {
     <h1 className="banner-text">Your avenue for Learning and Fun</h1>
   </div>
 
-  <div className="banner2">
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis explicabo vel, odit officia pariatur nisi! Impedit at, eos perferendis molestias sequi ipsam reprehenderit minima debitis ipsum maiores eum adipisci necessitatibus!</h1>
-    <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, consectetur nemo iste natus harum, provident alias repellendus dolore perferendis aut non suscipit, asperiores soluta. Eveniet nisi esse quod nam! Aliquid.</h3>
-</div>
+ <div className="banner2">
+    <h1>Eden playhouse provides exceptional care and education in a safe and nurturing environment for children 3 - 5 years old.</h1>
+    <h3>If you are interested in learning more about our program or how to enroll, please click on the button below to fill out our questionnaire and we will get back to you as soon as possible!</h3>
+  </div>
 <div>
  <a name="" id="" className="btn btn-primary" href="#" role="button"></a>
 </div>
