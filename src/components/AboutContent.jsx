04404913e5707bf4cc7b0666eaa26b04039ae9e9
Beu Bahala <beubahala@gmail.com>
2023-06-18 22:21:21 +0800

    reformat code add padding to mission

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import './AboutContent.css';
+import React from "react";
+import "./AboutContent.css";
 
 function AboutContent() {
-  return (<>
-    <div className="about">
-    <img src="5.png" alt="About Photo"/>
-    <h1 className="about-text">We believe that children are born eager and ready to learn!</h1>
-  </div>
-
-
-
-  <div className="mission">
-    <img src="mission.png" alt="mission photo"/>    
-    <h1>Our Mission Vision</h1>
-    <p>Eden Playhouse and Learning Center Inc. offers a quality education to be made avalilable <br />for every family to support parents to raise a god-fearing, well-rounded,<br /> disciplined, intelligent and talented children.</p>
-  </div>
-  
-  </>
-  )
+  return (
+    <>
+      <div className="about">
+        <img src="5.png" alt="About Photo" />
+        <h1 className="about-text">
+          We believe that children are born eager and ready to learn!
+        </h1>
+      </div>
+      <div className="mission">
+        <img src="mission.png" alt="mission photo" />
+        <h1>Our Mission Vision</h1>
+        <p>
+          Eden Playhouse and Learning Center Inc. offers a quality education to
+          be made avalilable <br />
+          for every family to support parents to raise a god-fearing,
+          well-rounded,
+          <br /> disciplined, intelligent and talented children.
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default AboutContent
+export default AboutContent;
